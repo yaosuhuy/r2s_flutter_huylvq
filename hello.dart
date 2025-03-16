9ddbbd6d2c2e627b51aefa4669197eabe22d3cfd
yaosuhuy <levanquanghuy8@gmail.com>
2025-03-16 11:44:34 +0700

    hello

---
+++ hello.dart
@@ -0,0 +1,10 @@
+void main() {
+  String name = "Lê Văn Quang Huy";
+  int age = 21;
+  String address = "Mai Lâm, Đông Anh, Hà Nội";
+
+  print("Thông tin cá nhân");
+  print("Tên: $name");
+  print("Tuổi: $age");
+  print("Địa chỉ: $address");
+}
